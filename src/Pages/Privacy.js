65a50faeb57769ded9e4122f2f490f06edb5f964
Ar1m4-is-the-GOAT <vatanojv@gmail.com>
2024-05-28 11:23:47 +0200

    Privacy Chi Siamo e Termini aggiornati + ho tolto il primo spazio in Header.css

--- src/Pages/Privacy.js
+++ src/Pages/Privacy.js
@@ -1,7 +1,38 @@
 export default function Privacy() {
+    const styles = {
+        div: {
+            margin: '30px',
+        },
+        p1: {
+            fontFamily: 'Times new Roman',
+            lineHeight: '1.5',
+        }
+    };
     return(
-        <div>
-            <h1>I tuoi dati verranno venduti a chiunque ci paghi a sufficienza, i cookie sono obbligatori</h1>
+        <div style={styles.div}>
+            <h1>I tuoi dati verranno venduti a chiunque ci paghi a sufficienza XD XD XD</h1>
+            <h3>A parte gli scherzi, cosa ne facciamo dei tuoi dati?</h3>
+            <p style={styles.p1}>
+            Presso RUBJ-THANOS, ci impegniamo a proteggere la tua privacy e a garantire la sicurezza delle informazioni che ci fornisci.<br></br>
+            Quando interagisci con il nostro sito web o utilizzi i nostri servizi, potremmo raccogliere diverse tipologie di informazioni,
+            inclusi dati personali, al fine di fornirti un'esperienza personalizzata e soddisfacente.<br></br><br></br>
+
+            Le informazioni che potremmo raccogliere includono, ma non sono limitate a:<br></br><br></br>
+
+            - <b>Informazioni di contatto</b>: nome, indirizzo e-mail, numero di telefono, indirizzo di spedizione.<br></br>
+            - <b>Informazioni di pagamento</b>: dati della carta di credito/debito, informazioni di fatturazione.<br></br>
+            - <b>Informazioni sulle interazioni</b>: dati di navigazione, cronologia degli acquisti, preferenze di prodotto.<br></br><br></br>
+
+            Utilizziamo queste informazioni per elaborare gli ordini, fornirti assistenza clienti, migliorare i nostri servizi e
+            personalizzare la tua esperienza di shopping.<br></br>
+            È importante sottolineare che raccogliamo solo le informazioni strettamente necessarie per soddisfare le tue richieste e 
+            non condividiamo mai i tuoi dati personali con terze parti senza il tuo consenso esplicito,
+            a meno che non sia richiesto per adempiere agli obblighi legali o per proteggere i nostri diritti, la tua sicurezza o la sicurezza degli altri.
+            Ti assicuriamo che implementiamo misure di sicurezza appropriate per proteggere le tue informazioni personali da accessi non autorizzati, 
+            perdite o alterazioni.
+            Le informazioni sensibili, come i dati di pagamento, vengono crittografate e trasmesse tramite protocolli sicuri per garantire la massima protezione
+            durante le transazioni online.
+            </p>
         </div>
     );
 }
--- src/Pages/Termini.js
+++ src/Pages/Termini.js
@@ -1,7 +1,46 @@
 export default function Termini() {
+    const styles = {
+        div: {
+            margin: '30px',
+        },
+        p1: {
+            fontFamily: 'Times new Roman',
+            lineHeight: '1.5',
+        }
+    };
     return(
-        <div>
-            <h1>Termini</h1>
+        <div style={styles.div}>
+            <h1>Termini e Condizioni d'Uso</h1>
+            <p style={styles.p1}>
+            Ti diamo il benvenuto su RUBJ-THANOS. Prima di utilizzare i nostri servizi, ti invitiamo a leggere attentamente i seguenti Termini e
+            Condizioni, che regolano l'accesso al nostro sito web e l'utilizzo dei nostri servizi.
+            Utilizzando il nostro sito, accetti di essere vincolato da questi Termini e Condizioni.
+            Se non sei d'accordo con una qualsiasi parte di questi Termini e Condizioni, ti preghiamo di non utilizzare il nostro sito.
+            <h3>Utilizzo del Sito</h3>
+            Il nostro sito e i suoi contenuti sono destinati esclusivamente a scopi informativi e di acquisto.
+            Non è consentito utilizzare il nostro sito per scopi illegali o non autorizzati.
+            Hai l'obbligo di fornire informazioni accurate e complete quando effettui un ordine attraverso il nostro sito.
+            <h3>Proprietà Intellettuale</h3>
+            Tutti i contenuti presenti sul nostro sito, inclusi testi, immagini, grafiche, loghi, video e marchi registrati, sono di nostra proprietà o
+            concessi in licenza e sono protetti dalle leggi sul copyright e sulla proprietà intellettuale.
+            Non è consentito copiare, riprodurre, distribuire o utilizzare in qualsiasi modo i contenuti del nostro sito senza il nostro consenso scritto.
+            <h3>Limitazioni di Responsabilità</h3>
+            
+
+            Facciamo del nostro meglio per garantire che le informazioni presenti sul nostro sito siano accurate e aggiornate,
+            ma non possiamo garantire la completezza o l'assenza di errori.
+            Utilizzi il nostro sito e i nostri servizi a tuo rischio. Non saremo responsabili per eventuali danni diretti, indiretti, incidental o
+            consequenziali derivanti dall'uso del nostro sito o della tua incapacità di utilizzare il sito.
+            <h3>Modifiche ai Termini e Condizioni</h3>
+            Ci riserviamo il diritto di modificare questi Termini e Condizioni in qualsiasi momento senza preavviso.
+            Le modifiche saranno efficaci immediatamente dopo la pubblicazione sul nostro sito.
+            Ti consigliamo di controllare periodicamente questa pagina per verificare eventuali modifiche.
+            L'uso continuato del nostro sito dopo la pubblicazione delle modifiche costituirà la tua accettazione di tali modifiche.
+            <h3>Legge Applicabile</h3>
+            Questi Termini e Condizioni sono regolati e interpretati in conformità con le leggi dello Stato Italiano.
+            Qualsiasi controversia derivante da o in relazione a questi Termini e
+            Condizioni sarà sottoposta alla giurisdizione esclusiva dei tribunali competenti nella giurisdizione dello Stato Italiano.
+            </p>
         </div>
     );
 }
